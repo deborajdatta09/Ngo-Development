0d40276ed3545078546d919fd6aa31d310cd9b41
Deboraj Datta <deborajdatta@gmail.com>
2024-04-09 04:32:25 +0600

    sorry message added to error page

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	<main id="tp_content" role="main">
 		<div id="primary" class="content-area">
 			<section class="error-404 not-found">
-				<h1 class="page-title text-center pt-3"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ngo-organization' ); ?></h1>
+				<h1 class="page-title text-center pt-3"><?php esc_html_e( 'Oops! We are sorry! That page can&rsquo;t be found.', 'ngo-organization' ); ?></h1>
 				<div class="page-content text-center">
 					<p class="py-3"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'ngo-organization' ); ?></p>
 					<?php get_search_form(); ?>
